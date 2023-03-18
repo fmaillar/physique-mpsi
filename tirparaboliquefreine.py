09b90c359b9c211d29aa2bdda63d55aeb6416402
Florian MAILLARD <florian.maillard.thebest@gmail.com>
2023-03-18 20:04:44 +0100

    Update email

--- tirparaboliquefreine.py
+++ tirparaboliquefreine.py
@@ -20,12 +20,12 @@ from labellines import labelLines
 #    return -k/m*vz-g
 def chute_frot(y, t, g, msurk, gamma):
     x, z, vx, vz = y        
-    return [vx, vz, -1/msurk*np.abs(vx)**gamma, -g-1/msurk*np.abs(vz)**gamma]
+    return [vx, vz, -ksurm*np.abs(vx)**gamma, -g-ksurm*np.abs(vz)**gamma]
 
 x0  = 0
 z0  = 0
-V0 = 19
-n = 7
+V0 = 20
+n = 8
 theta = (1/2-1/n)*np.pi
 thetadeg = np.ceil(theta * 180/np.pi)
 Vz0 = V0*np.sin(theta)
@@ -35,53 +35,42 @@ t = np.linspace(0, 30, 10000)
 g = 9.81
 #m = 10
 #k = 1
-msurk = 10
+ksurm = 0.1
 gamma = 1.5
 y0 = [x0, z0, Vx0, Vz0]
-
-
-
-# fig1 = plt.plot(x,z, '.')
-# plt.xlabel('$x$')
-# plt.ylabel('$y$')
-# plt.ylim([0, 25])
-# plt.xlim([0, 35])
-# plt.grid()
-#plt.tight_layout()
 
 #lines = ["-"]
 lines = ["-","--","-.",":"]
 linecycler = cycle(lines)
 
-sol0 = odeint(chute_frot, y0, t, args=(g,msurk,0))
+sol0 = odeint(chute_frot, y0, t, args=(g,ksurm,0))
 #index pour lequel z devient negatif
 END0 = np.where(sol0[:,1] < 0, sol0[:,1], -np.inf).argmax()
 
 cm = 1/2.54  # centimeters in inches
-figsize=(20*cm,15*cm)
-fig, ax = plt.subplots()
-plt.plot(sol0[:END0, 0], sol0[:END0, 1], label=r'$k=0$', ls=next(linecycler))
-for gamma in [1, 2]:
-    for msurk in [5.0, 2.0, 1.0]:    
-        sol = odeint(chute_frot, y0, t, args=(g,msurk,gamma))
+for gamma in [0.5, 0.707, 1]:
+    figsize=(20*cm,15*cm)
+    fig, ax = plt.subplots()
+    plt.plot(sol0[:END0, 0], sol0[:END0, 1], label=r'$k/m=0$', ls='-')
+    for ksurm in [0.1, 0.2, 0.5, 0.707, 1]:
+        sol = odeint(chute_frot, y0, t, args=(g,ksurm,gamma))
         END = 10+np.where(sol[:,1] < 0, sol[:,1], -np.inf).argmax()
-        plt.plot(sol[:END, 0], sol[:END, 1], label=f'$m/k={msurk:.0f},\\alpha={gamma:.0f}$',
+        plt.plot(sol[:END, 0], sol[:END, 1], 
+                 #label=f'$k/m={ksurm:.1f},\\alpha={gamma:.1f}$',
+                 label=f'$k/m={ksurm:.1f}$',
                  ls=next(linecycler))        
 
-plt.legend(loc='best')
-lines = ax.get_lines()
-xvals = [22, 18, 10.5, 20, 14, 7.46, 18, 11.53, 6.09, 30]
-#labelLines(lines, fontsize=10, xvals=xvals, yoffsets=0, zorder=5.5, align=True)
-plt.xlabel('$x$')
-plt.ylabel('$z$')
-plt.ylim([0, 20])
-plt.xlim([0, 35])
-props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-# plt.text(0.8,0.9, '$f = k v^{\\alpha}$', transform=ax.transAxes, fontsize=14,
-#         verticalalignment='top', bbox=props)
-plt.title(f"$V_0={V0} m/s$, $\\theta={thetadeg}$° selon les types de frottements $f$", bbox=props)
-plt.grid()
-plt.tight_layout()
-dpi=200
-plt.savefig('Tir_parabolique_frottements.png', dpi=dpi)
-plt.show()        
+    plt.legend(loc='best')
+    lines = ax.get_lines()
+    xvals = [22, 18, 10.5, 20, 14, 7.46, 18, 11.53, 6.09, 30]
+    plt.xlabel('$x$')
+    plt.ylabel('$z$')
+    plt.ylim([0, 20])
+    plt.xlim([0, 35])
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    plt.title(f"$V_0={V0} m/s$, $\\alpha={gamma}$, $\\theta={thetadeg}$° selon les types de frottements $f$", bbox=props)
+    plt.grid()
+    plt.tight_layout()
+    dpi=200
+    plt.savefig(f'Tir_parabolique_frottements_alpha_{gamma:.1f}.png', dpi=dpi)
+plt.close() 
--- tracé-chap17.py
+++ tracé-chap17.py
@@ -12,12 +12,15 @@ import matplotlib.pyplot as plt
 mpl.rcParams['text.usetex'] = True
 plt.rcParams.update({'font.size': 9})
 
-
 def fun(Omega, Q):
     xi = ((1-Omega**2)**2+Omega**2/Q**2)**(-0.5)
     phi = (np.arctan(Q*(1/Omega-Omega))-np.pi/2)*180/np.pi
     return [xi, phi, Omega]
 
+def fun2(Omega, Q):
+    xi = (Q**2*(Omega-1./Omega)**2+1)**(-0.5)
+    psi = np.arctan(Q*(1./Omega-Omega))*180/np.pi
+    return [xi, psi, Omega]
 
 Omega = np.linspace(0.01, 3, 300)
 XiMax = [(1-i**4)**(-0.5) for i in Omega if i < 1]
@@ -41,4 +44,31 @@ for i in range(Nsubfig):
     axs[i].legend(loc='best')
 plt.tight_layout()
 plt.savefig("Fig17.png")
-# plt.close(fig)
+plt.close(fig)
+
+List2 = [fun2(Omega, Q) for Q in Ql]
+Array2 = np.array(List2)
+label2 = ['$\\xi = \\frac{V}{V_M}$', '$\\Psi$ (deg)']
+fig, axs = plt.subplots(Nsubfig, 1, figsize=(6, 10), dpi=200)
+for i in range(Nsubfig):
+    for k in range(len(Ql)):
+        axs[i].plot(Omega, Array2[k, i, :], label=str(np.around(Ql[k], 3)))
+    axs[i].grid(True, which='both')
+    axs[i].set_ylabel(label2[i])
+    axs[i].set_xlabel("$\\Omega = \\frac{\\omega}{\\omega_0}$")
+    axs[i].legend(loc='best')
+plt.tight_layout()
+plt.savefig("Fig17-2.png")
+plt.close(fig)
+
+fig, ax = plt.subplots(1, 1, dpi=200)
+for k in range(len(Ql)):
+    plt.plot(Omega, Array2[k, 0, :]**2, label=str(np.around(Ql[k], 3)))
+ax.grid(True, which='both')
+ax.set_ylabel("$\\tau = \\frac{P}{P_M}$")
+ax.set_xlabel("$\\Omega = \\frac{\\omega}{\\omega_0}$")
+ax.legend(loc='best')
+plt.tight_layout()
+plt.savefig("Fig17-3.png")
+plt.close(fig)
+
