4c713704822639f9115ac942eb6081ec0600251f
Florian MAILLARD <florian.maillard.thebest@gmail.com>
2024-04-13 09:20:15 +0200

    le fichier est vide

--- puit_de_potentiel.py
+++
@@ -1,7 +0,0 @@
-"""Print the optic deviation."""
-#!/usr/bin/env python
-# coding: utf-8
-
-# from itertools import product
-from numpy import linspace, sin, arcsin, pi, arange
-import matplotlib.pyplot as plt
